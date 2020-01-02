1175f8c0ab1c44ccd5e766b588acfe7044dea8a5
Dunbar <15186@vtsd.com>
2020-01-02 11:38:23 -0500

    Forms stuff 1/2/20

---
+++ forms.js
@@ -0,0 +1,15 @@
+function refresher(){
+  var name = document.getElementById("name").value;
+  if(name.length < 5 || name.length > 25){
+    alert("Enter a longer name!!!");
+  }
+
+
+
+
+  <br>
+
+  <table id="myTable" border="1"
+    <tr>
+    <td>stuff</td>
+    <td>more stuff<
